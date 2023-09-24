2eec0036cf009a32e7719647267cb2ce9016ec60
Rafael <rafa.viana.moc@gmail.com>
2023-09-24 17:17:44 -0300

    Implementing done, and seems to work need tests!

--- ruffini.c
+++ ruffini.c
@@ -6,31 +6,16 @@
 
 typedef struct tgMetodoRuffini {
     char  *dividendo;           //- Dividendo
-    char  *divisor;               //- Divisor
     int  constDivisor;        //- Termo constante do divisor
-    int  constDividendo;    //- Termo constante do dividendo
     int  *coefDividendo;  //- Coef. do dividendo (a)x + bx
     int  numCoefficients;
 } DivisaoRuffine;
 
-void carregaConstantes(DivisaoRuffine *p) 
-{
-    int i = strlen(p->divisor - 1);
-    while(!isdigit(p->divisor[i]))
-        --i;
-    p->constDivisor = atoi(&p->divisor[i]);
-    
-    i = strlen(p->dividendo - 1);
-    while(!isdigit(p->dividendo[i]))
-        --i;
-    p->constDividendo = atoi(&p->dividendo[i]);
-}
-
 void carregaCoeficientes(DivisaoRuffine *p) 
 {
-    // Regular expression for finding coefficients in the dividend string
-    const char *pattern = "[+-]?[0-9]*x";
-
+    // Regular expression for finding coefficients and degrees in the dividend string
+    const char *pattern = "([+-]?[0-9]*x\\^[0-9]+|[+-]?[0-9]*x|[+-]?[0-9]+)";
+    
     regex_t regex;
     regmatch_t match;
 
@@ -41,19 +26,19 @@ void carregaCoeficientes(DivisaoRuffine *p)
     }
 
     char *str = p->dividendo;
-
     int size = 0;
+    
     while (regexec(&regex, str, 1, &match, 0) == 0) {
-        // Extract the matched coefficient
+        // Extract the matched coefficient or degree
         char *match_str = str + match.rm_so;
-
-        // If the coefficient is empty, assume it's 1
-        int coef = (match_str[0] == '+' || match_str[0] == '-') ? atoi(match_str) : 1;
+        int coef = (isdigit(match_str[0]) || (('+' == match_str[0] || 
+        '-' == match_str[0]) && isdigit(match_str[1]))) ? atoi(match_str) : 1;
 
         // Add the coefficient to your data structure
         ++size;
         p->coefDividendo = (int *)realloc(p->coefDividendo, size * sizeof(int));
-        p->coefDividendo[size - 1] = coef; p->numCoefficients = size;
+        p->coefDividendo[size - 1] = coef;
+        p->numCoefficients = size;
 
         // Move to the next part of the string
         str += match.rm_eo;
@@ -64,24 +49,48 @@ void carregaCoeficientes(DivisaoRuffine *p)
 
 void calcula(DivisaoRuffine *p) 
 {
+    if (p->coefDividendo == NULL || p->numCoefficients == 0) {
+        printf("No coefficients loaded for division.\n");
+        return;
+    }
 
+    int n = p->numCoefficients;
+    int *resultado = (int *)malloc(n * sizeof(int));
+
+    resultado[0] = p->coefDividendo[0];
+
+    for (int i = 1; i < n; ++i) {
+        resultado[i] = resultado[i - 1] * p->constDivisor;
+
+        resultado[i] += p->coefDividendo[i];
+    }
+
+    printf("Resultado dos coeficientes da divisao:\n");
+    for (int i = 0; i < n - 1; ++i) {
+        if(i < n - 2)
+            if(resultado[i] > 0 && i < n-2) printf("%dx^%d + ", resultado[i], (n - 2 - i));
+            else printf("%dx^%d", resultado[i], (n - 2 - i));
+        else printf("%d", resultado[i]);
+    }
+    printf("\n\nResto da divisao: %d\n\n", resultado[n - 1]);
+    free(resultado);
 }
 
 void metodoRuffini(DivisaoRuffine *p) 
 {
-    carregaConstantes(p);
     carregaCoeficientes(p);
     calcula(p);
 }
 
-int main ()
+int main() 
 {
-    DivisaoRuffine polinomios = { "x^2+4x+3", "x+1" };
+    DivisaoRuffine polinomios = { 
+        "3x^3-5x^2+x-2",
+        2,
+        NULL,
+        0
+    };
 
     metodoRuffini(&polinomios);
-
-    printf("Constante do Dividendo: %d\n", polinomios.constDividendo);
-    printf("Constante do Divisor: %d\n", polinomios.constDivisor);
-
-    for (int i = 0; i < polinomios.numCoefficients; ++ i) printf("%d\n", polinomios.coefDividendo[i]);
+    return 0;
 }
